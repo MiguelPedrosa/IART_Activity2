111615e5b674f3d8f94217e0161c0a34f3e964c2
MiguelPedrosa <pedrosa.miguel@gmail.com>
2019-04-18 02:12:34 +0100

    Added euclidean heuristic

--- src/algorithm/Heuristics.java
+++ src/algorithm/Heuristics.java
@@ -17,7 +17,7 @@ public class Heuristics {
         return differences;
     }
 
-    public static int manhattanDistance(Board a, Board b) {
+    public static int euclideanDistance(Board a, Board b) {
         int[][] aBoard = a.getBoard();
         int[][] bBoard = b.getBoard();
         double distance = 0;
@@ -36,6 +36,27 @@ public class Heuristics {
                 distance += pythagoras(aX, bX, aY, bY);
             }
         return (int) distance;
+    }
+
+    public static int manhattanDistance(Board a, Board b) {
+        int[][] aBoard = a.getBoard();
+        int[][] bBoard = b.getBoard();
+        double distance = 0;
+        for(int aY = 0; aY < aBoard.length; aY++)
+            for(int aX = 0; aX < aBoard[aY].length; aX++) {
+                final int searchNumber = aBoard[aY][aX];
+                int bX = 0;
+                int bY = 0;
+                // Find piece in board b;
+                for(int yIterator = 0; yIterator < bBoard.length; yIterator++)
+                    for(int xIterator = 0; xIterator < aBoard[yIterator].length; xIterator++)
+                        if(bBoard[yIterator][xIterator] == searchNumber) {
+                            bX = xIterator;
+                            bY = yIterator;
+                        }
+                distance += Math.abs(aX - bX) + Math.abs(aY - bY);
+            }
+        return distance;
     }
 
     private static double pythagoras(int x1, int x2, int y1, int y2) {
